f02b54f23e6af96fc0bc2d0b1bd96950f8210643
momistudioacuradimomi-droid <momistudioacuradimomi@gmail.com>
2026-03-25 13:41:18 +0100

    Delete files zivve directory

--- files zivve/sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE='zivve-v1';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html'])));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>{if(r)return r;return fetch(e.request).then(res=>{if(res&&res.status===200){const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));}return res;}).catch(()=>caches.match('/index.html'));}));});
-self.addEventListener('push',e=>{const data=e.data?e.data.json():{};e.waitUntil(self.registration.showNotification(data.title||'ZIVVE',{body:data.body||'Nuovo record da battere!',icon:'/icon-192.png',badge:'/icon-72.png',tag:'zivve-notif',renotify:true}));});
-self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.openWindow('/'));});
