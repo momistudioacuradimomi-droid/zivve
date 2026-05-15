ef09fd814b9a9e144027d8c87635f97b49189fda
MomiStudio <momistudioacuradimomi@gmail.com>
2026-05-15 23:34:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='zivve-20260406200056';
+// ZIVVE cache bump Patch 08
+const CACHE='zivve-202605142205-direct-small-game-text-hook';
 const ALWAYS_FRESH=['/zivve/','/zivve/index.html'];
 
 self.addEventListener('install',e=>{
@@ -77,3 +78,61 @@ self.addEventListener('notificationclick',e=>{
   e.notification.close();
   e.waitUntil(clients.openWindow('/zivve/'));
 });
+
+// patch-08c-cache-bust
+
+// PATCH_08D_CORRECT_SKIP_TARGET_FIX cache refresh
+
+// PATCH_08E_INTRO_SLOWER_TEXT_FADE cache refresh
+
+// PATCH_08F_COMMUNITY_OFFICIAL_LOGOS cache refresh
+
+// PATCH_08G_COMMUNITY_BUTTON_INTERIOR_MATCH cache refresh
+
+// STABLE_09_QA_BASE cache refresh
+
+// STEP_10A_AUDIO_HAPTIC_BASE cache refresh
+
+// PATCH_10A1_CHAMPION_AURA_VISUAL_HIDE cache refresh
+
+// STEP_10B_MOMENTI_CULT cache refresh
+
+// PATCH_10B1_GAMEOVER_STATS_LAYOUT_FIX cache refresh
+
+// PATCH_10B2_GAMEOVER_SCORE_FIT_FIX cache refresh
+
+// PATCH_10B3_GAMEOVER_HEADER_CLEANUP cache refresh
+
+// PATCH_10B4_GAMEOVER_AGAIN_FIX cache refresh
+
+// PATCH_10B5_HIDE_DASHED_MAGNET_RING cache refresh
+
+// PATCH_10B6_HIDE_GAMEPLAY_DASHED_GUIDES cache refresh
+
+// PATCH_10B7_ORBIT_RING_LEGACY_BANNER_CLEANUP cache refresh
+
+// STEP_11_VISUAL_POLISH_PASS cache refresh
+
+// PATCH_11A_REMOVE_LARGE_GLASS_RINGS cache refresh
+
+// STEP_12_INGAME_TEXT_POLISH cache refresh
+
+// STEP_12B_INGAME_TEXT_HARD_APPLY cache refresh
+
+// STEP_12C_INGAME_TEXT_VISIBLE_OVERLAY cache refresh
+
+// PATCH_12D_INGAME_TEXT_SIZE_TUNE cache refresh
+
+// PATCH_12E_INGAME_TEXT_SMALLER_REAL_TUNE cache refresh
+
+// PATCH_12F_HARD_SHRINK_INGAME_TEXT_OVERLAY cache refresh
+
+// PATCH_12G_REMOVE_INGAME_MESSAGE_CARDS cache refresh
+
+// PATCH_12H_DISABLE_INGAME_MESSAGE_OVERLAYS cache refresh
+
+// PATCH_12I_REMOVE_PRISMA_DASHED_RINGS cache refresh
+
+// PATCH_12J_SMALL_PREMIUM_INGAME_TEXT_RETURN cache refresh
+
+// PATCH_12K_DIRECT_SMALL_GAME_TEXT_HOOK cache refresh
